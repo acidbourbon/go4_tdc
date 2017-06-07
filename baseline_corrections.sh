24e2e8b89d0eb9c3d243dc532e3228700df87c98
Hades DAQ <hadaq@linux-l8n8.site>
2017-06-07 16:58:58 +0200

    added shellscript to adapt baselines

---
+++ baseline_corrections.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./baseline 0 0 10
+./baseline 0 0 2
+./baseline 0 0 5
+./baseline 0 0 8
+./baseline 0 0 -5
+./baseline 0 0 -6
+./baseline 0 3 1
+./baseline 0 4 -2
+./baseline 0 4 -3
+./baseline 0 5 -3
+./baseline 0 5 -5
+./baseline 0 7 -5
+./baseline 2 0 -3
+./baseline 2 0 -4
+./baseline 2 1 -2
+./baseline 2 3 -4
+./baseline 2 3 -5
+./baseline 2 4 -5
+./baseline 2 5 -2
+./baseline 2 6 -5
