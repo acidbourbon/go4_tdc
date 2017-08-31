5743990658941009dc2ae017cb94f338d4260eb4
Hades DAQ <hadaq@linux-l8n8.site>
2017-08-31 18:58:46 +0200

    modified go4.sh, so it can be applied to a directory containing data from one measurement split over multiple hld files

--- go4.sh
+++ go4.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 
-fname=$1
+#fname=$1
 i=$2 # number of triggers to plot
 
-dir=$(dirname $fname)
+#dir=$(dirname $fname)
+dir=$1
 
 
 {
-  . trb3login_x230
+  #. trb3login_x230
+  . my_trb3login
 
   rm Go4AutoSave.root
-  go4analysis -user $fname -num $i
-  mv Go4AutoSave.root $fname.root
+  go4analysis -user "$dir/*.hld" -num $i
+  mv Go4AutoSave.root $dir/go4_out.root
+  mv tree_out.root $dir/
   cp second.C $dir/second.C_bak
 }
 
