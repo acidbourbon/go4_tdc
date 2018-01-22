29f314297e88509c3d6bcb1d4e82bde3548924cb
Michael Wiebusch <m.wiebusch@gsi.de>
2018-01-22 13:11:49 +0100

    modified unpack_range.sh so it works with the offline data storage path

--- unpack_range.sh
+++ unpack_range.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for i in $(seq $@); do
-	        ./go4_on_all.sh l8n8_data/$(printf "%03d*" $i)
+	        #./go4_on_all.sh l8n8_data/$(printf "%03d*" $i)
+	        ./go4_on_all.sh elements_data/$(printf "%03d*" $i)
 	done
 
